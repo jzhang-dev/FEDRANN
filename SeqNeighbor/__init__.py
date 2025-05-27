6d4c786c18538c0adcc21680a910d29e40cc6da6
Jia-Yuan Zhang <zhangjiayuan@genomics.cn>
2025-05-27 17:36:46 +0800

    bump version to v0.1.2

--- SeqNeighbor/__init__.py
+++ SeqNeighbor/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-__version__ = "v0.1.1"
+__version__ = "v0.1.2"
 __description__ = f"seqNeighbor {__version__}: a software finding reads' nearest neighbors rapidly."
 __url__ = ""
