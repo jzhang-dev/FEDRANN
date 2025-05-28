f0f513242f11be494b562d5e67ef1773c547fa63
Jia-Yuan Zhang <zhangjiayuan@genomics.cn>
2025-05-28 14:10:45 +0800

    fix CI

--- test/test.sh
+++ test/test.sh
@@ -5,21 +5,11 @@ set -exuo pipefail
 rm -rf test/output/
 mkdir -p test/output/
 
-SeqNeighbor encode \
-    -q test/data/reads.fasta.gz \
-    -t test/data/reads.fasta.gz \
-    -o test/output/feature_extraction \
-    -p IDF \
+SeqNeighbor \
+    -i test/data/reads.fasta.gz \
+    -o test/output/ \
     -k 15 \
     --kmer-sample-fraction 0.05 \
-    --kmer-sample-seed 602 \
+    --seed 602 \
     --kmer-min-multiplicity 2
 
-
-SeqNeighbor qvt \
-    -i test/output/feature_extraction \
-    -d SparseRP \
-    -n 500 \
-    --knn-method NNDescent \
-    -o test/output/KNN \
-    --n-neighbors 20 
