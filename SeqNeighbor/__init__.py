2f7c5536369ee052e1e31582981226ca7fc9fed1
Jia-Yuan Zhang <zhangjiayuan@genomics.cn>
2025-05-30 12:27:25 +0800

    v0.3.4

--- SeqNeighbor/__init__.py
+++ SeqNeighbor/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-__version__ = "v0.3.3"
+__version__ = "v0.3.4"
 __description__ = f"seqNeighbor {__version__}: a software finding reads' nearest neighbors rapidly."
 __url__ = ""
