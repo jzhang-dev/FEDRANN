000cbf099aa6df8e5e6416f19ab4971746f06590
Jia-Yuan Zhang <56027973+jzhang-dev@users.noreply.github.com>
2026-01-27 10:01:26 +0800

    v0.5.4

--- fedrann/__init__.py
+++ fedrann/__init__.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 
 
-__version__ = "v0.5.3"
-__description__ = f""
-__url__ = ""
+__version__ = "v0.5.4"
+__description__ = f"Fedrann: a scalable pipeline for overlap detection based on large-scale sequencing data."
+__url__ = "https://github.com/jzhang-dev/FEDRANN"
